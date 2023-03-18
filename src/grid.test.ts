f4265bfe74953ef74539e95ca92c653f97533635
Author Name <amyvpellegrini@gmail.com>
2023-03-18 21:57:58 +0100

    feat(grid): initialise and organise cells

---
+++ src/grid.test.ts
@@ -0,0 +1,48 @@
+import { describe, it, test, expect } from "vitest";
+import Cell from "./cell";
+import Grid from "./grid";
+
+describe("grid", () => {
+  it("can be created with rows and columns", () => {
+    const grid = new Grid(10, 5);
+
+    expect(grid.rows).toEqual(10);
+    expect(grid.columns).toEqual(5);
+  });
+
+  it("prepares the grid", () => {
+    const grid = new Grid(2, 2);
+
+    const cell1 = new Cell(0, 0);
+    const cell2 = new Cell(0, 1);
+    const cell3 = new Cell(1, 0);
+    const cell4 = new Cell(1, 1);
+
+    cell3.north = cell1;
+    cell4.north = cell2;
+    cell1.east = cell2;
+    cell3.east = cell4;
+    cell2.west = cell1;
+    cell4.west = cell3;
+    cell1.south = cell3;
+    cell2.south = cell4;
+
+    expect(grid.grid).toEqual([
+      [cell1, cell2],
+      [cell3, cell4],
+    ]);
+  });
+
+  it("configures the cells", () => {
+    const grid = new Grid(2, 2);
+
+    expect(grid.grid[1][0].north).toEqual(grid.grid[0][0]);
+    expect(grid.grid[1][1].north).toEqual(grid.grid[0][1]);
+    expect(grid.grid[0][0].east).toEqual(grid.grid[0][1]);
+    expect(grid.grid[1][0].east).toEqual(grid.grid[1][1]);
+    expect(grid.grid[0][1].west).toEqual(grid.grid[0][0]);
+    expect(grid.grid[1][1].west).toEqual(grid.grid[1][0]);
+    expect(grid.grid[0][0].south).toEqual(grid.grid[1][0]);
+    expect(grid.grid[0][1].south).toEqual(grid.grid[1][1]);
+  });
+});
---
+++ src/grid.ts
@@ -0,0 +1,52 @@
+import Cell from "./cell";
+
+type Row = Cell[];
+
+export default class Grid {
+  rows: number;
+  columns: number;
+  grid: Row[];
+  cells: Cell[];
+
+  constructor(rows: number, columns: number) {
+    this.rows = rows;
+    this.columns = columns;
+    this.cells = [];
+    this.grid = this.prepareGrid();
+    this.configureCells();
+  }
+
+  prepareGrid() {
+    const grid = Array.from({ length: this.rows });
+
+    return grid.map((_, rowIdx) => {
+      const row = Array.from({ length: this.columns });
+
+      return row.map((_, colIdx) => {
+        const cell = new Cell(rowIdx, colIdx);
+        this.cells.push(cell);
+        return cell;
+      });
+    });
+  }
+
+  configureCells() {
+    this.cells.forEach((cell) => {
+      if (this.grid[cell.row - 1]) {
+        cell.north = this.grid[cell.row - 1][cell.column];
+      }
+
+      if (this.grid[cell.row + 1]) {
+        cell.south = this.grid[cell.row + 1][cell.column];
+      }
+
+      if (this.grid[cell.row][cell.column + 1]) {
+        cell.east = this.grid[cell.row][cell.column + 1];
+      }
+
+      if (this.grid[cell.row][cell.column - 1]) {
+        cell.west = this.grid[cell.row][cell.column - 1];
+      }
+    });
+  }
+}

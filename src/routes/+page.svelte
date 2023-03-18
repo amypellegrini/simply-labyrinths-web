106c57d5f0f6d0c0da5feab8e2bfaa308de270bc
Author Name <amyvpellegrini@gmail.com>
2023-03-19 00:35:32 +0100

    feat: render grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Grid from '../grid';
+
+	const maze = new Grid(10, 10);
+
+	const cellSize = 30;
+</script>
+
+<h1>Simply Labyrinths</h1>
+<div class="grid">
+	{#each maze.cells as cell}
+		<div class="cell" style="left: {cell.column * cellSize}px; top: {cell.row * cellSize}px" />
+	{/each}
+</div>
+
+<style>
+	h1 {
+		text-align: center;
+	}
+
+	.grid {
+		margin: 3rem auto;
+		border: 1px solid black;
+		width: 300px;
+		height: 300px;
+		position: relative;
+	}
+
+	.cell {
+		position: absolute;
+		box-sizing: border-box;
+		width: 30px;
+		height: 30px;
+		border: 1px solid black;
+	}
+</style>

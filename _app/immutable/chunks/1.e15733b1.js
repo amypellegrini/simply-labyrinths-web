c1de309266cede6983f41394f9b59d15413f2aa1
Amy Pellegrini <amyvpellegrini@gmail.com>
2023-04-05 10:32:00 +0200

    Updates

---
+++ _app/immutable/chunks/1.e15733b1.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.96a68a6e.js";export{t as component};
--- _app/immutable/chunks/2.2528d527.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.48e4fb82.js";export{t as component};

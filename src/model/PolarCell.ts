1e4699b211541a6570e4e589f962ce3f63032863
Amy Pellegrini <amyvpellegrini@gmail.com>
2023-04-14 15:49:12 +0200

    fix(PolarGrid): decouple from base class as they cannot be compatible

--- src/model/PolarCell.ts
+++ src/model/PolarCell.ts
@@ -1,12 +1,101 @@
-import Cell from './cell';
+import Distances from './distances';
 
-export default class PolarCell extends Cell {
-	clockwise: number | undefined;
-	counterClockwise: number | undefined;
-	inwards: PolarCell | undefined;
+interface CellLink {
+	cell: PolarCell;
+	bidi: boolean;
+}
+
+export default class PolarCell {
+	id: string;
+	row: number;
+	column: number;
+	links: Map<string, CellLink>;
+	clockwise: PolarCell;
+	counterClockwise: PolarCell;
+	inwards: PolarCell;
 	outwards: PolarCell[] = [];
 
 	constructor(row: number, column: number) {
-		super(row, column);
+		this.clockwise = this;
+		this.counterClockwise = this;
+		this.inwards = this;
+		this.id = `${row}-${column}`;
+		this.row = row;
+		this.column = column;
+		this.links = new Map();
+	}
+
+	link(cell: PolarCell, bidi = false) {
+		if (!this.links.has(cell.id)) {
+			this.links.set(cell.id, {
+				cell,
+				bidi
+			});
+
+			if (bidi) {
+				cell.link(this, true);
+			}
+		}
+	}
+
+	unlink(cell: PolarCell) {
+		this.links.delete(cell.id);
+
+		if (cell.links.has(this.id)) {
+			cell.unlink(this);
+		}
+	}
+
+	linked(cell: PolarCell) {
+		return this.links.has(cell.id);
+	}
+
+	neighbours() {
+		const list: PolarCell[] = [];
+
+		if (this.clockwise) {
+			list.push(this.clockwise);
+		}
+
+		if (this.counterClockwise) {
+			list.push(this.counterClockwise);
+		}
+
+		if (this.inwards) {
+			list.push(this.inwards);
+		}
+
+		if (this.outwards) {
+			list.push(...this.outwards);
+		}
+
+		return list;
+	}
+
+	distances(): Distances {
+		const distances = new Distances(this);
+		let frontier: PolarCell[] = [this];
+
+		while (frontier.length > 0) {
+			const newFrontier: PolarCell[] = [];
+
+			frontier.forEach((cell) => {
+				cell.links.forEach((linked) => {
+					if (distances.cells.get(linked.cell) !== undefined) {
+						return;
+					}
+
+					distances.setCellDistance(
+						linked.cell,
+						(distances.cells.get(cell) as number) + 1
+					);
+					newFrontier.push(linked.cell);
+				});
+			});
+
+			frontier = newFrontier;
+		}
+
+		return distances;
 	}
 }
--- src/model/PolarGrid.ts
+++ src/model/PolarGrid.ts
@@ -1,9 +1,21 @@
-import Grid from './grid';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import type Cell from './cell';
 import PolarCell from './PolarCell';
 
-export default class PolarGrid extends Grid {
+type Row = PolarCell[];
+
+export default class PolarGrid {
+	rows: number;
+	columns: number;
+	grid: Row[];
+	cells: PolarCell[];
+
 	constructor(rows: number) {
-		super(rows, 1);
+		this.rows = rows;
+		this.columns = 1;
+		this.cells = [];
+		this.grid = this.prepareGrid();
+		this.configureCells();
 	}
 
 	prepareGrid() {
@@ -34,7 +46,32 @@ export default class PolarGrid extends Grid {
 		return rows;
 	}
 
+	isPolarCell(cell: Cell): cell is PolarCell {
+		return (
+			'clockwise' in cell &&
+			'counterClockwise' in cell &&
+			'inwards' in cell &&
+			'outwards' in cell
+		);
+	}
+
 	configureCells() {
-		console.log('configureCells');
+		const cells = this.cells as unknown as PolarCell[];
+
+		cells.forEach((cell: PolarCell) => {
+			const row = cell.row;
+			const col = cell.column;
+			if (row > 0) {
+				cell.clockwise = this.grid[row][col + 1];
+				cell.counterClockwise = this.grid[row][col - 1];
+
+				const ratio = this.grid[row].length / this.grid[row - 1].length;
+				const parent = this.grid[row - 1][Math.floor(col / ratio)];
+
+				parent.outwards.push(cell);
+
+				cell.inwards = parent;
+			}
+		});
 	}
 }

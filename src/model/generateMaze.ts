4f3f76496c070e517e2cea4fb3a77478bf1a96b2
Amy Pellegrini <amyvpellegrini@gmail.com>
2023-04-24 12:55:22 +0200

    fix(generateMaze): remove sidewinder from generation algo

--- src/model/generateMaze.ts
+++ src/model/generateMaze.ts
@@ -1,12 +1,11 @@
 import recursiveBacktracker from './recursiveBacktracker';
 import wilson from './wilson';
 import huntAndKill from './huntAndKill';
-import sidewinder from './sidewinder';
 import random from './random';
 import type Grid from './grid';
 
 export default function generateMaze(grid: Grid) {
-	const algos = [recursiveBacktracker, wilson, huntAndKill, sidewinder];
+	const algos = [recursiveBacktracker, wilson, huntAndKill];
 
 	return algos[random(0, algos.length - 1)](grid);
 }
